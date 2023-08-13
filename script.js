3c9f3aac08b5cad383bdac8531da9f0bff3c7064
Kkaiyuanwg <118371002+Kkaiyuanwg@users.noreply.github.com>
2023-08-13 20:23:07 +0200

    added new cursor

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const proyectos = document.querySelectorAll('.proyectos');
 
+let currentX = 0;
+let currentY = 0;
+let followSpeed = 0.1;
+
+function updateCirclePosition(targetX, targetY) {
+    gsap.to(circle, {
+      duration: 0.7, // Adjust this duration for the follow speed
+      x: targetX - 15,
+      y: targetY - 15,
+      ease: 'power3.out' // Adjust the easing for desired effect
+    });
+  }
+
 document.addEventListener('mousemove', (e) => {
     const centerX = window.innerWidth / 2;
     const centerY = window.innerHeight / 2;
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
+    
+    const x = e.clientX;
+    const y = e.clientY;
+  
+    updateCirclePosition(x - 15, y - 15);
 
-    const deltaX = (mouseX - centerX) * 0.01; // Ajusta el factor para controlar la intensidad del efecto
-    const deltaY = (mouseY - centerY) * 0.01;
+    circle.style.display = 'block';
+
+    const deltaX = (x - centerX) * 0.01; // Ajusta el factor para controlar la intensidad del efecto
+    const deltaY = (y - centerY) * 0.01;
 
     proyectos.forEach((proyecto) => {
         const parallaxX = deltaX * -1;
@@ -17,9 +35,19 @@ document.addEventListener('mousemove', (e) => {
     });
 });
 
+window.addEventListener('mouseout', () => {
+    circle.style.display = 'none';
+  });
+
+window.addEventListener('scroll', () => {
+    document.getElementById("movtxt").setAttribute('scroll', window.scrollY / (document.body.offsetHeight - window.innerHeight));
+    console.log(window.scrollY / (document.body.offsetHeight - window.innerHeight))
+}, false);
+
 document.addEventListener("DOMContentLoaded", function () {
 
     const secciones = document.querySelectorAll(".contenido");
+    const textTransition = document.querySelector(".game-section h2");
 
     function mostrarSeccion(seccionIndex) {
         secciones.forEach((seccion, index) => {
